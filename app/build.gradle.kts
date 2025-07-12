3947ea13a5f214bbb43af1715588ba2731d74513
Ouky22 <72163389+Ouky22@users.noreply.github.com>
2025-07-12 21:30:31 +0200

    chore: add initial hilt setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -61,4 +63,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // dagger hilt
+    implementation(libs.hilt.android)
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 }
---
+++ app/src/main/java/com/example/grocerly/GrocerlyApp.kt
@@ -0,0 +1,9 @@
+package com.example.grocerly
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GrocerlyApp : Application() {
+
+}
