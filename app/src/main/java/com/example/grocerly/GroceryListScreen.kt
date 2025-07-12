a10eb7bd463d96765858598a1a61a6d09e45dfc0
Ouky22 <72163389+Ouky22@users.noreply.github.com>
2025-07-12 19:57:45 +0200

    add initial files

---
+++ app/src/main/java/com/example/grocerly/GroceryListScreen.kt
@@ -0,0 +1,12 @@
+package com.example.grocerly
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun GroceryListScreen(
+    modifier: Modifier,
+) {
+
+}
---
+++ app/src/main/java/com/example/grocerly/GroceryListViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.grocerly
+
+
+class GroceryListViewModel {
+}
